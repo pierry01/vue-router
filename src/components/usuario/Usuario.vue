af4c72fc2427bf6d4908d0164934f347cfa4062c
Jean Mendonça <pierrybm@gmail.com>
2021-02-03 14:50:19 -0300

    navigate with code

--- src/components/usuario/Usuario.vue
+++ src/components/usuario/Usuario.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="usuario">
     <h2>Usuários</h2>
+    <hr />
+    <button sucesso @click="irParaInicio">Início</button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    irParaInicio() {
+      this.$router.push("/");
+    },
+  },
+};
 </script>
 
 <style>
